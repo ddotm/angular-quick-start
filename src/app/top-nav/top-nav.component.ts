27a8ae59a44f435086848e7b094da65a17e2f88d
Dmitri Mogilevski <dmogilevski@gmail.com>
2020-09-03 15:57:46 -0500

    sign in, sign out

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import Oidc from 'oidc-client';
+import {AuthService} from '../common/services/auth.service';
 
 @Component({
   selector: 'aqs-top-nav',
@@ -6,10 +8,26 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./top-nav.component.scss']
 })
 export class TopNavComponent implements OnInit {
+  public userProfile: Oidc.Profile = null;
 
-  constructor() {
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.authService.getUser();
+    this.authService.$userProfile.pipe()
+        .subscribe({
+          next: (userProfile: Oidc.Profile) => {
+            this.userProfile = userProfile;
+          }
+        });
+  }
+
+  public signIn(): void {
+    this.authService.login();
+  }
+
+  public signOut(): void {
+    this.authService.logout();
   }
 }
